8a8cc3b628233aa00cd6f8ae4b911780528f932c
natbitton54 <nathanieldavidbitton54@gmail.com>
2023-04-20 14:01:05 -0400

    I updated act() method in Target class

--- MyWorld.java
+++ MyWorld.java
@@ -10,13 +10,14 @@ public class MyWorld extends World
 {
     private long lastFrameTimeMS;
     private double timeStepDuration;
-    
+
     public MyWorld()
     {    
         super(768, 576, 1, false);
-        
+
         lastFrameTimeMS = System.currentTimeMillis();
         timeStepDuration = 1.0 / 60; // seems to be the default
+        prepare();
     }
 
     public void started()
@@ -29,10 +30,27 @@ public class MyWorld extends World
         timeStepDuration = (System.currentTimeMillis() - lastFrameTimeMS) / 1000.0;
         lastFrameTimeMS = System.currentTimeMillis();
     }
-    
+
     public double getTimeStepDuration()
     {
         return timeStepDuration;
     }
-    
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Cannon cannon = new Cannon();
+        addObject(cannon,76,496);
+        Target target = new Target();
+        addObject(target,544,480);
+        Target target2 = new Target();
+        addObject(target2,500,361);
+        Target target3 = new Target();
+        addObject(target3,422,265);
+        Target target4 = new Target();
+        addObject(target4,303,174);
+    }
 }
--- Target.java
+++ Target.java
@@ -1,4 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.*;
 
 /**
  * Write a description of class Target here.
@@ -14,6 +15,22 @@ public class Target extends Actor
      */
     public void act()
     {
-        // Add your action code here.
+        List<CannonBall> balls = getWorld().getObjects(CannonBall.class);
+        
+        for(int i = 0; i < balls.size(); i++){
+            CannonBall ball = balls.get(i);
+            
+            Vector2D targetToBall = new Vector2D(ball.getX() - getX(), ball.getY()-getY());
+            double distance = targetToBall.magnitude();
+            
+            double ballRadius = ball.getImage().getHeight() / 2;
+            double targetRadius = getImage().getHeight() / 2;
+            
+            if(distance < ballRadius + targetRadius){
+                setImage("targetDestroyed.png");
+                Greenfoot.playSound("explosion.wav");
+            }
+            
+        }
     }
 }
